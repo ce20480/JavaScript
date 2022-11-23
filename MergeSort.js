c8d7e430ffbd2197ac5a29dfb14a0b48b5543392
ce20480 <ce20480@bristol.ac.uk>
2022-11-23 18:37:01 +0000

    Fixed errors and produced intended output
    applied properties wrong and led to undefined arrays which led to the properties producing a type error

--- MergeSort.js
+++ MergeSort.js
@@ -1,26 +1,35 @@
 function MergeSort(arr){
-    if (arr.length < 2){
-        return arr;
+    if (Array.isArray(arr)){
+        if (arr.length < 2){
+            return arr;
+        }
+        const midIndex = Math.floor(arr.length/2);
+        const leftArr = arr.slice(0,midIndex);
+        const rightArr = arr.slice(midIndex,arr.length);
+    
+        return merge(MergeSort(leftArr), MergeSort(rightArr))
     }
-    const midIndex = Math.floor(arr.length/2);
-    const leftArr = arr.slice[0,midIndex];
-    const rightArr = arr.slice[midIndex,arr.length];
-
-    return merge(MergeSort(leftArr), MergeSort(rightArr))
 }
 
 function merge(leftArr, rightArr){
-    let resultArr = [];
-    let leftIndex = 0;
-    let rightIndex = 0;
-
-    while (leftIndex < leftArr.length && rightIndex < rightArr.length){
-        if (leftArr[leftIndex] < rightArr[rightIndex]){
-            resultArr.push(leftArr[leftIndex])
-            leftIndex += 1
-        } else{
-            resultArr.push(rightArr[rightIndex])
-            rightIndex += 1
+    if (Array.isArray(leftArr) && Array.isArray(rightArr)){
+        let resultArr = [];
+        let leftIndex = 0;
+        let rightIndex = 0;
+    
+        while (leftIndex < leftArr.length && rightIndex < rightArr.length){
+            if (leftArr[leftIndex] < rightArr[rightIndex]){
+                resultArr.push(leftArr[leftIndex])
+                leftIndex += 1
+            } else{
+                resultArr.push(rightArr[rightIndex])
+                rightIndex += 1
+            }
         }
+        return resultArr.concat(leftArr.slice(leftIndex)).concat(rightArr.slice(rightIndex))//forgot to add a line to account for the last element in one of the lists
     }
 }
+
+let arr = [12, 3, 16, 6, 5, 1];
+console.log(MergeSort(arr))
+console.log(Array.isArray(arr))
