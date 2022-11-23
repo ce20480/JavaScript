fa9f01032f56b74731e3f6920c18c80ad6ee2d12
ce20480 <ce20480@bristol.ac.uk>
2022-11-23 18:53:37 +0000

    Comments to understand in future

--- MergeSort.js
+++ MergeSort.js
@@ -1,35 +1,35 @@
 function MergeSort(arr){
-    if (Array.isArray(arr)){
-        if (arr.length < 2){
-            return arr;
-        }
-        const midIndex = Math.floor(arr.length/2);
-        const leftArr = arr.slice(0,midIndex);
-        const rightArr = arr.slice(midIndex,arr.length);
-    
-        return merge(MergeSort(leftArr), MergeSort(rightArr))
+    // if (Array.isArray(arr)){
+    if (arr.length < 2){
+        return arr;
     }
+    // divide and conquer approach keeps recursively calling mergesort until there is an array of length 1 and then builds the sorted list with the function merge
+    const midIndex = Math.floor(arr.length/2);
+    const leftArr = arr.slice(0,midIndex);
+    const rightArr = arr.slice(midIndex,arr.length);
+
+    return merge(MergeSort(leftArr), MergeSort(rightArr))
+    // }
 }
 
 function merge(leftArr, rightArr){
-    if (Array.isArray(leftArr) && Array.isArray(rightArr)){
-        let resultArr = [];
-        let leftIndex = 0;
-        let rightIndex = 0;
-    
-        while (leftIndex < leftArr.length && rightIndex < rightArr.length){
-            if (leftArr[leftIndex] < rightArr[rightIndex]){
-                resultArr.push(leftArr[leftIndex])
-                leftIndex += 1
-            } else{
-                resultArr.push(rightArr[rightIndex])
-                rightIndex += 1
-            }
+    // if (Array.isArray(leftArr) && Array.isArray(rightArr)){ this line is only useful if dealing with undefined values/arrays 
+    let resultArr = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+
+    while (leftIndex < leftArr.length && rightIndex < rightArr.length){
+        if (leftArr[leftIndex] < rightArr[rightIndex]){
+            resultArr.push(leftArr[leftIndex])
+            leftIndex += 1
+        } else{
+            resultArr.push(rightArr[rightIndex])
+            rightIndex += 1
         }
-        return resultArr.concat(leftArr.slice(leftIndex)).concat(rightArr.slice(rightIndex))//forgot to add a line to account for the last element in one of the lists
     }
+    return resultArr.concat(leftArr.slice(leftIndex)).concat(rightArr.slice(rightIndex))//forgot to add a line to account for the last element in one of the lists
+    // }
 }
 
-let arr = [12, 3, 16, 6, 5, 1];
+let arr = [12, 3, 16, 6, 5, 1];// test output
 console.log(MergeSort(arr))
-console.log(Array.isArray(arr))
